2bb57e8008c0173e97ade267bde928495c71a0cf
magicenvision <rjrose2003@gmail.com>
2026-08-10 16:41:08 -0400

    Initial Commit

---
+++ Learning_Login_Signup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learning_Login_Signup
+//
+//  Created by magic on 2026-08-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learning_Login_Signup/Learning_Login_SignupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_Login_SignupApp.swift
+//  Learning_Login_Signup
+//
+//  Created by magic on 2026-08-10.
+//
+
+import SwiftUI
+
+@main
+struct Learning_Login_SignupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
