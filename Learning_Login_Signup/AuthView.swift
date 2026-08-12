801ce97312b112e20418aae50586351f3cc88ae0
magicenvision <rjrose2003@gmail.com>
2026-08-11 23:25:04 -0400

    1:44 https://www.youtube.com/watch?v=0jOZAR9MxdY created enum AuthType which is staged for authentication using login by default. Created 3 different views.

---
+++ Learning_Login_Signup/AuthView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Learning_Login_Signup
+//
+//  Created by magic on 2026-08-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   
+    }
+}
+
+Struct
+
+#Preview {
+    ContentView()
+}
